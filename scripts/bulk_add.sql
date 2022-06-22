4efb0ccf38b133fc30403adbf1dd05c32ccb0cab
Rosamund <rsmnd.mthr@gmail.com>
2022-06-22 07:53:04 +0000

    Add bulk add query

---
+++ scripts/bulk_add.sql
@@ -0,0 +1,16 @@
+/*
+TO RUN QUERY:
+sqlite3 <path-to-db> ".read bulk_add.sql"
+*/
+
+INSERT INTO vocab (de_word, en_transl)
+VALUES
+("   ", "   "),
+("   ", "   "),
+("   ", "   "),
+("   ", "   "),
+("   ", "   "),
+("   ", "   ");
+
+SELECT "Vocab added successfully!";
+
